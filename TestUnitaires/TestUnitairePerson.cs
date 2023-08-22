558f8acf9f5d1d88f1abfe40942f59aa5dc53917
BOUNATIROU Rodolphe <RBounatirou@arfp.asso.fr>
2023-08-22 14:33:39 +0200

    Classe test unitaire Person ok

--- TestUnitaires/TestUnitairePerson.cs
+++ TestUnitaires/TestUnitairePerson.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using src;
 
 namespace TestUnitaires
 {
@@ -8,7 +9,51 @@ namespace TestUnitaires
         [TestMethod]
         public void Person_Constructeur_Nom_BOUNATIROU_Prenom_Rodolphe_InstanciationOk()
         {
-            
+            Person p = new Person("BOUNATIROU", "Rodolphe");
+            Assert.IsTrue(p.Nom == "BOUNATIROU", "Nom incorrect");
+            Assert.IsTrue(p.Prenom == "Rodolphe", "Prenom incorrect");
+        }
+
+        [TestMethod]
+        public void Person_Constructeur_Nom_null_Prenom_Rodolphe_Exception()
+        {
+            Assert.ThrowsException<Exception>(()=>new Person(null, "Rodolphe"),
+                "Pas d'exception a l'instanciation d'une personne avec un nom null");
+        }
+
+        [TestMethod]
+        public void Person_Constructeur_Nom_BOUNATIROU_Prenom_null_Exception()
+        {
+            Assert.ThrowsException<Exception>(() => new Person("BOUNATIROU", null),
+                "Pas d'exception a l'instanciation d'une personne avec un prenom null");
+        }
+
+        [TestMethod]
+        public void Person_Constructeur_Nom_chainevide_Prenom_Rodolphe_Exception()
+        {
+            Assert.ThrowsException<Exception>(() => new Person("", "Rodolphe"),
+                "Pas d'exception a l'instanciation d'une personne avec un nom vide");
+        }
+
+        [TestMethod]
+        public void Person_Constructeur_Nom_BOUNATIROU_Prenom_chainevide_Exception()
+        {
+            Assert.ThrowsException<Exception>(() => new Person("BOUNATIROU", ""),
+                "Pas d'exception a l'instanciation d'une personne avec un prenom vide");
+        }
+
+        [TestMethod]
+        public void Person_Constructeur_Nom_chaineEspace_Prenom_Rodolphe_Exception()
+        {
+            Assert.ThrowsException<Exception>(() => new Person(" ", "Rodolphe"),
+                "Pas d'exception a l'instanciation d'une personne avec un nom uniquement composé d'espace");
+        }
+
+        [TestMethod]
+        public void Person_Constructeur_Nom_BOUNATIROU_Prenom_chaineEspace_Exception()
+        {
+            Assert.ThrowsException<Exception>(() => new Person("BOUNATIROU", " "),
+                "Pas d'exception a l'instanciation d'une personne avec un prenom uniquement composé d'espace");
         }
     }
 }
