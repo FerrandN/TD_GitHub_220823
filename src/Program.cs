02fb5bc23fa28a34a01ef67ee728c05ca9153a3d
BOUNATIROU Rodolphe <RBounatirou@arfp.asso.fr>
2023-08-22 11:01:21 +0200

    Ajout lecture/ecriture nom+prenom dans program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Entrez votre nom");
+        string nom = LireDonee();
+        Console.WriteLine("Entrez votre prenom");
+        string prenom = LireDonee();
+        Console.WriteLine(String.Format("{0} {1}", prenom, nom));
+
+    }
+
+    private static string LireDonee()
+    {
+        string val = "";
+        do
+        {
+            val = Console.ReadLine().Trim();
+        } while (val.Equals(""));
+        return val;
+    }
+}
