dfcbce13efdff2f62898f47841b0f71027ac5cbe
BOUNATIROU Rodolphe <RBounatirou@arfp.asso.fr>
2023-08-22 11:22:09 +0200

    besoin de pull

---
+++ src/PersonContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class PersonContainer : IPersonContainer
+    {
+        List<Person> persons;
+
+        public PersonContainer(List<Person> initialPersons)
+        {
+
+        }
+        public List<Person> SortByFirstName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> SortByLastName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
